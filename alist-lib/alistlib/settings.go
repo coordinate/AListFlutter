7d682562ebdcb957522378a33647eb93b3fd02a1
zxy <coordinate@live.cn>
2024-11-29 01:15:47 +0800

    ...

--- alist-lib/alistlib/settings.go
+++ alist-lib/alistlib/settings.go
@@ -1,10 +1,10 @@
 package alistlib
 
 import (
-	"github.com/alist-org/alist/v3/cmd"
-	"github.com/alist-org/alist/v3/cmd/flags"
-	"github.com/alist-org/alist/v3/internal/op"
-	"github.com/alist-org/alist/v3/pkg/utils"
+	"github.com/coordinate/alist/cmd"
+	"github.com/coordinate/alist/cmd/flags"
+	"github.com/coordinate/alist/internal/op"
+	"github.com/coordinate/alist/pkg/utils"
 )
 
 func SetConfigData(path string) {
